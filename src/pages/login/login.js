907cfb19cc2e28e3f84dbae94ccc54789f6aec41
Shruti Jain <jainshruti408@gmail.com>
2025-06-17 14:42:20 +0530

    changes made

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,20 +2,20 @@ import '../common.css'
 export function Login() {
     return (
         <>
-            <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title heading">Signin to your PopX account</h5>
-                    <h6 class="card-subtitles" style={{top: '109px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</h6>
+            <div className="card" >
+                <div className="card-body">
+                    <h5 className="card-title heading">Signin to your PopX account</h5>
+                    <h6 className="card-subtitles" style={{top: '109px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</h6>
                     <form action="/user">
-                        <div class="form-group">
+                        <div className="form-group">
                             <label className="input-title" style={{ top: '185px', width: '96px', color: '#6C25FF' }} for="exampleInputEmail1">Email Address</label>
-                            <input type="text" class="form-control input-login" style={{ top: '194px' }} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter email address' />
+                            <input type="text" className="form-control input-login" style={{ top: '194px' }} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter email address' />
                         </div>
-                        <div class="form-group">
+                        <div className="form-group">
                             <label className="input-title" style={{ top: '248px', width: '95px', color: '#6C25FF' }} for="exampleInputPassword1">Password</label>
-                            <input type="text" class="form-control input-login" style={{ top: '257px' }} id="exampleInputPassword1" placeholder="Enter password" />
+                            <input type="text" className="form-control input-login" style={{ top: '257px' }} id="exampleInputPassword1" placeholder="Enter password" />
                         </div>
-                        <button type="submit" class="btn link" style={{top: '311px', background:'#CBCBCB', color: '#FFFFFF'}}>Login</button>
+                        <button type="submit" className="btn link" style={{top: '311px', background:'#CBCBCB', color: '#FFFFFF'}}>Login</button>
                     </form>
                 </div>
             </div>
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -5,41 +5,41 @@ export function Register() {
             <div className="card" >
                 <div className="heading">Create your PopX account</div>
                 <form action='/user'>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label className="input-title" style={{ top: '101px', width: '96px', color: '#6C25FF' }} for="exampleIn,putEmail1">Full Name</label>
-                        <input type="text" class="form-control input" style={{ top: '110px' }} id="exampleInputEmail1" aria-describedby="emailHelp" defaultValue="Marry Doe" required />
+                        <input type="text" className="form-control input" style={{ top: '110px' }} id="exampleInputEmail1" aria-describedby="emailHelp" defaultValue="Marry Doe" required />
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label className="input-title" style={{ top: '165px', width: '95px', color: '#6C25FF' }} for="exampleInputPassword1">Phone number</label>
-                        <input type="text" class="form-control input" style={{ top: '174px' }} id="exampleInputPassword1" defaultValue="Marry Doe" required />
+                        <input type="text" className="form-control input" style={{ top: '174px' }} id="exampleInputPassword1" defaultValue="Marry Doe" required />
                     </div>
 
-                    <div class="form-group">
+                    <div className="form-group">
                         <label className="input-title" style={{ top: '229px', width: '98px', color: '#6C25FF' }} for="exampleInputPassword1">Email address</label>
-                        <input type="text" class="form-control input" style={{ top: '238px' }} id="exampleInputPassword1" defaultValue="Marry Doe" required />
+                        <input type="text" className="form-control input" style={{ top: '238px' }} id="exampleInputPassword1" defaultValue="Marry Doe" required />
                     </div>
 
-                    <div class="form-group">
+                    <div className="form-group">
                         <label className="input-title" style={{ top: '293px', width: '68px', color: '#6C25FF' }} for="exampleInputPassword1">Password</label>
-                        <input type="text" class="form-control input" style={{ top: '302px' }} id="exampleInputPassword1" defaultValue="Marry Doe" required />
+                        <input type="text" className="form-control input" style={{ top: '302px' }} id="exampleInputPassword1" defaultValue="Marry Doe" required />
                     </div>
 
-                    <div class="form-group">
+                    <div className="form-group">
                         <label className="input-title" style={{ top: '357px', width: '95px',color: '#6C25FF' }} for="exampleInputPassword1">Company name</label>
-                        <input type="text" class="form-control input" style={{ top: '366px' }} id="exampleInputPassword1" defaultValue="Marry Doe" />
+                        <input type="text" className="form-control input" style={{ top: '366px' }} id="exampleInputPassword1" defaultValue="Marry Doe" />
                     </div>
-                    <fieldset class="form-group" >
-                        <legend class="input-title" style={{top: '421px', left: '20px', width: '125px', color: '#1D2226'}}>Are you an agency?</legend>
-                        <div class="form-check form-check-inline" style={{position: 'absolute', top: '447px', left: '20px', width: '23px', height: '17px'}}>
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" checked />
-                            <label class="form-check-label" for="inlineRadio1">Yes</label>
+                    <fieldset className="form-group" >
+                        <legend className="input-title" style={{top: '421px', left: '20px', width: '125px', color: '#1D2226'}}>Are you an agency?</legend>
+                        <div className="form-check form-check-inline" style={{position: 'absolute', top: '447px', left: '20px', width: '23px', height: '17px'}}>
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" checked />
+                            <label className="form-check-label" for="inlineRadio1">Yes</label>
                         </div>
-                        <div class="form-check form-check-inline" style={{position: 'absolute', top: '447px', left: '80px', width: '23px', height: '17px'}}>
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                            <label class="form-check-label" for="inlineRadio2">No</label>
+                        <div className="form-check form-check-inline" style={{position: 'absolute', top: '447px', left: '80px', width: '23px', height: '17px'}}>
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                            <label className="form-check-label" for="inlineRadio2">No</label>
                         </div>
                     </fieldset>
-                    <button type="submit" class="btn link" style={{ background: '#6C25FF', top: '530px',  color: '#FFFFFF'}} >Create Account</button>
+                    <button type="submit" className="btn link" style={{ background: '#6C25FF', top: '530px',  color: '#FFFFFF'}} >Create Account</button>
                 </form>
             </div>
         </>
